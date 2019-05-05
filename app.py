72b2f00073a7df733847f05bd278b9858245dd0e
rony <imaginaryrony@gmail.com>
2019-05-05 14:11:28 +0600

    updated

--- app.py
+++ app.py
@@ -1,3 +1,34 @@
-from flask import Flask
-from flask import render_template,request,redirect,
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+posts = [
+
+    {
+        'author': 'kamal khan',
+        'title':'it is a first title',
+        'content':'it is a first content',
+        'date_posted':'may 5,2019'
+    },
+
+     {
+        'author': 'jamal khan',
+        'title':'it is a second title',
+        'content':'it is a second content',
+        'date_posted':'may 6,2019'
+    }
+]
+
+@app.route('/home')
+def home():
+    return render_template('home.html',variable_poll=posts)
+print(home)
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title='about')
+
+if __name__=='__main__':
+    app.run(debug=True)
+
 
