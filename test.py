9e48fae6c485a6a5bd464482481f4d5e320dd32b
rony <imaginaryrony@gmail.com>
2019-05-15 10:53:50 +0600

    practise lamda function complete

--- test.py
+++ test.py
@@ -56,8 +56,15 @@ while i<11:
         break
     i+=1'''
 
-x=lambda a,b:a*b
-print(x(3,6))
+
+'''x=lambda a,b,c:(a*b)+c
+print(x(9,3,7))'''
+
+def myfunc(n):
+    return lambda a:a*n
+
+mydoubler=myfunc(2)
+print(mydoubler(3))
 
 
 
